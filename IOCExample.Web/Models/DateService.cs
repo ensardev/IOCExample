6fd62d9dead08269bae2e77d97cd694de640977d
ensarakkuzey <ensarakkuzey@gmail.com>
2022-06-08 14:16:48 +0300

    Add Web App and create DateService interface for example.

---
+++ IOCExample.Web/Models/DateService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace IOCExample.Web.Models
+{
+    public class DateService : ISingletonDataService, IScopedDateService, ITransientDateService
+    {
+        private readonly ILogger<DateService> _logger;
+
+        public DateService(ILogger<DateService> logger)
+        {
+            _logger = logger;
+
+            _logger.LogWarning("DateService instance created.");
+        }
+
+        public DateTime GetDateTime { get; } = DateTime.Now;
+    }
+}
---
+++ IOCExample.Web/Models/IDateService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IOCExample.Web.Models
+{
+    public interface IDateService
+    {
+        DateTime GetDateTime { get; }
+    }
+
+    interface ISingletonDataService : IDateService { }
+    interface IScopedDateService : IDateService { }
+    interface ITransientDateService : IDateService { }
+}
